1339fe1873b87f76540d1664cf79b5ad3413d254
Rahul Sharma <120258636+rahulnitsxr2016@users.noreply.github.com>
2022-12-10 19:12:26 +0530

    Update CheckDiskSpace.sh

--- my automation/CheckDiskSpace.sh
+++ my automation/CheckDiskSpace.sh
@@ -4,5 +4,5 @@ THRESHOLD=90
 
 if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
 	echo "sending email!";
-	/usr/local/bin/sendEmail  -f PROTEX@sita.aero -t cathal.mclaughlin@sita.aero,gary.doherty@sita.aero -s 57.250.243.10 -u "Disk Space LOW!" -m "Your root partition remaining free space is critically low. Used: $CURRENT%";
+	/usr/local/bin/sendEmail  -f PROTEX@companydomain-t rahul@xyz.com-s 57.250.243.10 -u "Disk Space LOW!" -m "Your root partition remaining free space is critically low. Used: $CURRENT%";
 fi
